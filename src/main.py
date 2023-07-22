af1ed2a7ee329f0271438a8481e8a6c95571f9e8
lumes-master <Lumes119@gmail.com>
2023-07-22 12:25:41 +0300

    added jinja templates and path for static files in template

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.encoders import jsonable_encoder
+from starlette.routing import Mount
 
 from game_logic import outer_game_function
+# routs = [Mount("/static", StaticFiles(directory="static"), name="static")]
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.websocket("/ws")
 async def websocket_bullscow(websocket: WebSocket):
@@ -14,3 +21,7 @@ async def websocket_bullscow(websocket: WebSocket):
         game = game1(data)
         game_json = jsonable_encoder(game)
         await websocket.send_json(game_json)
+
+@app.get("/", response_class=HTMLResponse)
+async def main(request:Request):
+    return templates.TemplateResponse('frontpage.html', {'request':request})
